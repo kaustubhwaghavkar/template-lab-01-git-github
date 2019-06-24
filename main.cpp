f8e671c0da2f4be61b9228a95848d36f324e3b0e
houman eskandani <HoumanEskandani>
2019-06-24 10:51:56 -0700

    Add initial program
    * Added an initial main file which prints hello git to the command line

--- main.cpp
+++ main.cpp
@@ -1,14 +1,8 @@
 #include <iostream>
-#include<string>
-int main()
+
+int main ()
 {
-	std::cout<<"Now I am adding the input name from the user!"	
-	std::string name;
-	std::cout<<"What is your name?"<<std:endl;
-	std::cin>>name;
-	std::out<<"Hello "<<name<<"!"<<std::endl;
-	std::cout<<":::Below outputs are from previous exercises:::"<<std::endl;
-	std::cout<< "hello git"<< std::endl;
-	std::cout<<"git is easy?"<<std endl;
-	return 0;
+	std::cout << "hello git" << std::endl;
+	return 1;
 }
+
